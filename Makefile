85e27f69229673ac0839a41e2e84ed013fa04a07
lavanya <lavanya@geekbull.in>
2017-03-27 17:03:50 +0530

    web socket handler using cowboy

---
+++ Makefile
@@ -0,0 +1,5 @@
+PROJECT = websocket_cowboy
+PROJECT_DESCRIPTION = New project
+PROJECT_VERSION = 0.1.0
+DEPS = cowboy
+include erlang.mk
---
+++ _rel/websocket_cowboy_release/lib/websocket_cowboy-0.1.0/src/websocket_cowboy_app.erl
@@ -0,0 +1,25 @@
+-module(websocket_cowboy_app).
+-behaviour(application).
+
+-export([start/2]).
+-export([stop/1]).
+
+start(_Type, _Args) ->
+	Dispatch = cowboy_router:compile([
+		{'_', [
+			{"/", cowboy_static, {priv_file, websocket_cowboy, "index.html"}},
+			{"/websocket_cowboy", web_socket, []}
+		]}
+	]),
+	{ok, _} = cowboy:start_http(http, 100, [{port, 8080}], 
+	[
+		{compress, true},
+		{env, [{dispatch, Dispatch}]}
+		
+		
+	]),
+	io:format("hi"),
+	websocket_cowboy_sup:start_link().
+
+stop(_State) ->
+ok.
